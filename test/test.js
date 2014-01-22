100c4a6532c53e488157c6bd95e2fb5a8e2d44f0
Jamie Collinson <jamiecollinson@gmail.com>
2014-01-22 19:48:52 +0000

    more testing

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
-var assert = require("assert");
+var should = require("should");
 
 describe('built in functions', function() {
-  it('should know true is true is true', function() {
-    assert.equal(true, true);
+  it('should know true is true', function() {
+    true.should.be.true;
+  });
+  it('should know how to add', function() {
+    (1+1).should.equal(2);
   });
 });

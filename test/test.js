20fb9b81afe591683eeef0d1b9775c3a98791068
Jamie Collinson <jamiecollinson@gmail.com>
2014-01-23 13:57:33 +0000

    sort out package.json

--- test/test.js
+++ test/test.js
@@ -1,10 +1,9 @@
-var should = require("should");
+var should = require('should');
 
-describe('built in functions', function() {
-  it('should know true is true', function() {
-    true.should.be.true;
-  });
-  it('should know how to add', function() {
-    (1+1).should.equal(2);
+var Boids = require('../index.js');
+
+describe('boids.js', function() {
+  it('should exist', function() {
+    Boids.should.exist;
   });
 });
